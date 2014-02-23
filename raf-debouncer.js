8b23ea24fc922ddbe6bee6bcec58f65b679bd426
Mike Pjura <michael.pjura@gmail.com>
2014-02-23 12:08:58 -0500

    onResize, onScroll events.. on events.. first crack at state

--- raf-debouncer.js
+++ raf-debouncer.js
@@ -1,7 +1,7 @@
 var rafDebounce = (function( window ){
   "use strict";
 
-  window.requestAnimationFrame = (function(){
+  var raf = (function(){
     return  window.requestAnimationFrame       ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame    ||
@@ -10,6 +10,64 @@ var rafDebounce = (function( window ){
             };
   })();
 
-  
+  var lastScrollY = 0,
+      lastWidth   = window.innerWidth,
+      lastHeight  = window.innerHeight;
+
+  var state = {};
+
+  for ( var i = 0, e = ["scroll","resize"]; i < e.length; i++ ) {
+    state[ e[i] ] = {
+      listening: false,
+      ticking: false,
+      queue: {}
+    };
+  }
+
+
+  function onResize( evt ) {
+    lastWidth = window.innerWidth;
+    lastHeight = window.innerHeight;
+
+    if ( ! state.resize.ticking ) {
+      processQueue("resize", [ evt, lastWidth, lastHeight ] );
+    }
+  }
+
+  function onScroll( evt ) {
+    lastScrollY = window.scrollY;
+
+    if ( ! state.scroll.ticking ) {
+      processQueue("scroll", [ evt, lastScrollY ] );
+    }
+  }
+
+  function processQueue( type, args ) {
+    var queue = state[ type ].queue;
+
+
+  }
+
+  return {
+    on: function( type, key, callback ) {
+      if ( typeof key === "function" ) {
+        callback = key;
+        key = "anon";
+      }
+
+      var s = state[ type ];
+
+      if ( ! s.queue[ key ] ) {
+        s.queue[ key ] = [];
+      }
+
+      s.queue[ key ].push( callback );
+    },
+    off: function( type, key ) {
+      if ( typeof key !== "string" ) {
+
+      }
+    }
+  };
 
 })( this );
