ed451144be1bf80d2745f9a540a2ca022ba6cc5a
Mike Pjura <michael.pjura@gmail.com>
2014-02-23 13:34:08 -0500

    added uglify, added demo stubs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,28 @@ module.exports = function( grunt ) {
 
   grunt.initConfig({
 
+    pkg: grunt.file.readJSON('package.json'),
+    
     jshint: {
       files: [ "*.js" ]
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - <%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          "raf-debouncer.min.js": "raf-debouncer.js"
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
   grunt.registerTask("test", ["jshint"] );
+  grunt.registerTask("build", ["jshint", "uglify"] );
 };
